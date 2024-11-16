609ce7869f040195bec98cf938b56ee99a3b137b
eXDEcoder34 <exdecoder.global@gmail.com>
2024-11-16 23:49:10 +0300

    task 3 file

---
+++ Week_2_PLP/task_3.py
@@ -0,0 +1,11 @@
+# Task 3
+personal_info = {}
+
+personal_info["name"] = input("Enter name:")
+
+personal_info["age"] = input("Enter age:")
+
+personal_info["favorite-color"] = input("Enter favorite-color:")
+
+
+print(personal_info)
