bda7e2dbab019a60a55408beb1a89779dcb452e5
eXDEcoder34 <exdecoder.global@gmail.com>
2024-11-17 00:24:40 +0300

    updated file

--- Week_2_PLP/answers.py
+++ Week_2_PLP/answers.py
@@ -1,5 +1,7 @@
+# Task 1
 my_list = [] 
 
+# Task 2
 my_list.append(10)
 my_list.append(20)
 my_list.append(30)
@@ -7,6 +9,12 @@ my_list.append(40)
 
 print(my_list)
 
+# Task 3
 my_list.insert(1, 15)
+
+print(my_list)
+
+# Task 4
+my_list.extend([50, 60, 70])
 
 print(my_list)
