bd673efa80081b957e868ae9c8ebfb5bd8c9252c
eXDEcoder34 <exdecoder.global@gmail.com>
2024-11-16 22:02:05 +0300

    Assignment complete

---
+++ calculate.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+
+calculation = input("Enter the calculation (+): ") 
+
+if calculation == "+":
+    result = num1 + num2
+    print(f"{num1} + {num2} = {result}")
+
+
